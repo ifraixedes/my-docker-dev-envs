8a60d3d28f3a211e0e64baf9ec489863fe56e7d2
Ivan Fraixedes <ivan@fraixed.es>
2019-10-16 13:42:54 +0200

    images: Add setup script hyperfine tool
    Add a new setup script to install hyperfine command-line benchmarking
    tool and install them in 'ifc' and 'storj' images.

---
+++ images/setup-scripts/99-hyperfine.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+  rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+  -o "${tmp_dir}/hyperfine.tgz" \
+  "https://github.com/sharkdp/hyperfine/releases/download/v1.8.0/hyperfine-v1.8.0-x86_64-unknown-linux-gnu.tar.gz"
+
+cd "${tmp_dir}"
+tar zxf "hyperfine.tgz" --strip-components 1
+chmod +x "hyperfine"
+mv hyperfine "${installation_dir}/hyperfine"
