0db98bb7c82ea6f7efa7055aa826b3052ea4d06b
Ivan Fraixedes <ivan@fraixed.es>
2020-03-08 21:10:39 +0100

    images: Install exercism in ifc image
    Add the setup script for installing exercism CLI (exercism.io),
    programming language exercises for learning them, and install it in the
    ifc Docker image.

---
+++ images/setup-scripts/99-exercism.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S bash -
+
+set -eu -o pipefail
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+  rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+  -o "${tmp_dir}/exercism.tgz" \
+  "https://github.com/exercism/cli/releases/download/v3.0.13/exercism-linux-64bit.tgz"
+
+cd "${tmp_dir}"
+tar zxf "exercism.tgz"
+chmod +x "exercism"
+mv exercism "${installation_dir}/exercism"
