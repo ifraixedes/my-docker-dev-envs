6276e5a08f80f010fc1dc1d755343ed374122af8
Ivan Fraixedes <ivan@fraixed.es>
2020-04-05 20:41:03 +0200

    images: Install hugo to ifc image
    Create a setup script to install Hugo static site generator and install
    it in the ifc image.

---
+++ images/setup-scripts/99-hugo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S bash -
+
+set -eu -o pipefail
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+  rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+  -o "${tmp_dir}/hugo.tgz" \
+  "https://github.com/gohugoio/hugo/releases/download/v0.68.3/hugo_0.68.3_Linux-64bit.tar.gz"
+
+tar -C "${installation_dir}" --no-same-owner -xzf "${tmp_dir}/hugo.tgz"
