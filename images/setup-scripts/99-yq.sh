eb97c412552ac510a8474e765918bd7a8bf5e3f4
Ivan Fraixedes <ivan@fraixed.es>
2020-04-09 15:38:43 +0200

    images: Install yq to Storj docker image
    Add the setup script to install yq and install it in the Storj docker
    image.

---
+++ images/setup-scripts/99-yq.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S bash -
+
+set -eu -o pipefail
+
+installation_dir=/apps
+mkdir -p "${installation_dir}"
+
+curl --fail -L \
+  -o "${installation_dir}/yq" \
+  "https://github.com/mikefarah/yq/releases/download/3.2.1/yq_linux_amd64"
+
+chmod +x "${installation_dir}/yq"
