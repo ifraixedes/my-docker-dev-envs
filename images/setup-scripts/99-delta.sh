99ca841aa7b0334802a6f099d198ef26b5dad12b
Ivan Fraixedes <ivan@fraixed.es>
2020-05-28 16:16:53 +0200

    images: Install delta tool in workspace images
    Install the delta diffting tool in the workspace docker images (ifc and
    Storj).

---
+++ images/setup-scripts/99-delta.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S bash -
+
+set -eu -o pipefail
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+  rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+  -o "${tmp_dir}/delta.tgz" \
+  "https://github.com/dandavison/delta/releases/download/0.1.1/delta-0.1.1-x86_64-unknown-linux-musl.tar.gz"
+
+cd "${tmp_dir}"
+tar -zxf "delta.tgz" --strip-components 1 --no-same-owner
+chmod +x "delta"
+mv delta "${installation_dir}/delta"
