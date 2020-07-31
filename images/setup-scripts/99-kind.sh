9b48f179af3fb110cf7380c5f41f225948285dc4
Ivan Fraixedes <ivan@fraixed.es>
2020-07-31 13:02:45 +0200

    images/setup-scripts: Update kind version
    Update the Kind version to be installed

--- images/setup-scripts/99-kind.sh
+++ images/setup-scripts/99-kind.sh
@@ -7,5 +7,5 @@ mkdir -p ${installation_dir}
 
 curl --fail -L \
   -o "${installation_dir}/kind" \
-  "https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-linux-amd64"
+  "https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-linux-amd64"
 chmod +x "${installation_dir}"/kind
