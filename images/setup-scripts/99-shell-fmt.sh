22f91c4052f8966efb4a7ea6b796adc120e14355
Ivan Fraixedes <ivan@fraixed.es>
2020-08-19 17:36:44 +0200

    images: Install shfmt tool in workspace images
    Install the shell formatter tool in the workspace docker images (ifc and
    Storj).

---
+++ images/setup-scripts/99-shell-fmt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S bash -
+
+set -eu -o pipefail
+
+version=3.1.2
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+curl --fail -L \
+  -o "${installation_dir}/shfmt" \
+  "https://github.com/mvdan/sh/releases/download/v${version}/shfmt_v${version}_linux_amd64"
+
+chmod +x "${installation_dir}/shfmt"
