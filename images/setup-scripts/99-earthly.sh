30d6d0df6ec40608f6e06d78e71b8cc81d28c9a5
Ivan Fraixedes <ivan@fraixed.es>
2020-10-06 16:08:32 +0200

    images: Install watchexec & earthly tool in ifc image
    Install the watchexec and earthly tool in the ifc docker image.

---
+++ images/setup-scripts/99-earthly.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S bash -
+
+set -eu -o pipefail
+
+version=0.3.8
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+curl --fail -L \
+	-o "${installation_dir}/earthly" \
+	"https://github.com/earthly/earthly/releases/download/v${version}/earth-linux-amd64"
+
+chmod +x "${installation_dir}/earthly"
