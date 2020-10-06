c68c1bb75d46dd6cc75e9ca82977c465a7cbab32
Ivan Fraixedes <ivan@fraixed.es>
2020-10-06 16:08:32 +0200

    images: Install watchexec tool in ifc image
    Install the watchexec tool in the ifc docker image.

---
+++ images/setup-scripts/99-watchexec.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+version=1.14.1
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+	rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${tmp_dir}/watchexec.tgz" \
+	"https://github.com/watchexec/watchexec/releases/download/${version}/watchexec-${version}-x86_64-unknown-linux-musl.tar.xz"
+
+cd "${tmp_dir}"
+tar -xf "watchexec.tgz" --strip-components 1 --no-same-owner
+chmod +x "watchexec"
+mv watchexec "${installation_dir}/watchexec"
