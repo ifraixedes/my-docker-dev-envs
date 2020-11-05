a4ad221943ec6fa00d30573ad3a99fa5b5539296
Ivan Fraixedes <ivan@fraixed.es>
2020-11-05 13:49:55 +0100

    images/setup-sripts/storj: Add helm v3 installation
    Add the setup script for installing helm v3 and rename the already
    existing helm (v2) setup script to contain v2 in its filename.
    
    Currently we use have to keep both version until we finally have
    migrated completely to helm v3.

---
+++ images/setup-scripts/storj/70-helm-v3.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env -S bash -
+
+set -eu -o pipefail
+
+helm_version="3.4.0"
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+	rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${tmp_dir}/helm.tgz" \
+	"https://get.helm.sh/helm-v${helm_version}-linux-amd64.tar.gz"
+
+curl --fail -L \
+	-o "${tmp_dir}/helm.sha256" \
+	"https://get.helm.sh/helm-v${helm_version}-linux-amd64.tar.gz.sha256"
+
+cd "${tmp_dir}"
+sha=$(cat helm.sha256)
+calculated_sha=$(sha256sum helm.tgz | cut -d ' ' -f 1)
+if [[ "${sha}" != "${calculated_sha}" ]]; then
+	echo "Invalid Helm checksum"
+	exit 1
+fi
+
+tar --no-same-owner -xzf helm.tgz
+chmod +x linux-amd64/helm
+mv linux-amd64/helm ${installation_dir}/helm3
