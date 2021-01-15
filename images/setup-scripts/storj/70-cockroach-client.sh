65e87cd535e756baadd942c56c07180b84e56c52
Ivan Fraixedes <ivan@fraixed.es>
2021-01-15 12:24:49 +0100

    image: Install cockroach client Storj image
    Install the cockroach client on the Storj work environment image.

---
+++ images/setup-scripts/storj/70-cockroach-client.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+version=20.2.3
+
+installation_dir=/apps
+mkdir -p "${installation_dir}"
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+	rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${tmp_dir}/cockroach.tgz" \
+	"https://binaries.cockroachdb.com/cockroach-v${version}.linux-amd64.tgz "
+
+cd "${tmp_dir}"
+tar -zxf "cockroach.tgz" --strip-components 1 --no-same-owner
+chmod +x "${tmp_dir}/cockroach"
+mv cockroach "${installation_dir}/cockroach"
