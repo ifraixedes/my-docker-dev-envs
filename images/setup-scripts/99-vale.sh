4b2812dad4f2e8c5e96a7917331d3260a8622534
Ivan Fraixedes <ivan@fraixed.es>
2021-02-04 08:33:07 +0100

    images/setup-scripts: Homogenize some scripts
    User `version` for a variable name rather than `version_to_install` and
    add a blank line between curl and the unpacking and installing commands.

--- images/setup-scripts/99-vale.sh
+++ images/setup-scripts/99-vale.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-version_to_install="1.7.1"
+version="1.7.1"
 
 installation_dir=/apps
 mkdir -p ${installation_dir}
@@ -15,7 +15,8 @@ trap cleanup EXIT
 
 curl --fail -L \
 	-o "${tmp_dir}/vale.tgz" \
-	"https://github.com/errata-ai/vale/releases/download/v${version_to_install}/vale_${version_to_install}_Linux_64-bit.tar.gz"
+	"https://github.com/errata-ai/vale/releases/download/v${version}/vale_${version}_Linux_64-bit.tar.gz"
+
 tar -C "${tmp_dir}" --no-same-owner -xzf "${tmp_dir}/vale.tgz"
 mv "${tmp_dir}/vale" "${installation_dir}"
 chmod +x "${installation_dir}/vale"
--- images/setup-scripts/storj/99-velero.sh
+++ images/setup-scripts/storj/99-velero.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env -S bash -
+#!/usr/bin/env bash
 
 set -eu -o pipefail
 
-version_to_install="1.4.2"
+version="1.4.2"
 
 installation_dir=/apps
 mkdir -p ${installation_dir}
@@ -15,7 +15,7 @@ trap cleanup EXIT
 
 curl --fail -L \
 	-o "${tmp_dir}/velero.tgz" \
-	"https://github.com/vmware-tanzu/velero/releases/download/v${version_to_install}/velero-v${version_to_install}-linux-amd64.tar.gz"
+	"https://github.com/vmware-tanzu/velero/releases/download/v${version}/velero-v${version}-linux-amd64.tar.gz"
 
 cd "${tmp_dir}"
 tar -zxf "velero.tgz" --strip-components 1 --no-same-owner
