0e218b7ce256380c6dbec4fe0bdc617d09e0f6dd
Ivan Fraixedes <ivan@fraixed.es>
2021-03-08 19:25:25 +0100

    images: Don't install kustomize & install packer
    Don't Install kustomize because it's shipped with Kubectl for quite a
    while and install packer.

---
+++ images/setup-scripts/storj/70-packer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+version=1.6.6
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+	rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${tmp_dir}/packer.zip" \
+	"https://releases.hashicorp.com/packer/${version}/packer_${version}_linux_amd64.zip"
+
+unzip -j "${tmp_dir}/packer.zip" -d "${installation_dir}"
+chmod +x "${installation_dir}/packer"
