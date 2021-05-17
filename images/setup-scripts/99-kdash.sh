8b3a65bd88155ba48acf697ed456afd103d49966
Ivan Fraixedes <ivan@fraixed.es>
2021-05-17 19:34:45 +0200

    images: Add kdash to Storj image
    Add a new setup script to install kdash and install it in the Storj
    workspace image.

---
+++ images/setup-scripts/99-kdash.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+readonly VERSION="0.1.2"
+readonly INSTALLATION_DIR="/apps"
+mkdir -p "${INSTALLATION_DIR}"
+
+readonly BIN_NAME="kdash"
+readonly DOWNLOAD_URL="https://github.com/kdash-rs/kdash/releases/download/v${VERSION}/kdash-linux-musl.tar.gz"
+
+TMP_DIR=$(mktemp -d)
+readonly TMP_DIR
+cleanup() {
+	trap - EXIT
+
+	rm -rf "${TMP_DIR}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${TMP_DIR}/${BIN_NAME}.tgz" \
+	"${DOWNLOAD_URL}"
+
+cd "${TMP_DIR}"
+tar zxf "${BIN_NAME}.tgz"
+chmod +x "${BIN_NAME}"
+mv "${BIN_NAME}" "${INSTALLATION_DIR}/"
