b987785fae56c45dfe20ef3bd4ede95a7ad02f32
Ivan Fraixedes <ivan@fraixed.es>
2021-06-23 17:46:54 +0200

    images: Add curlie to Storj image
     Add a new setup script to install curlie and install it in the Storj
     workspace image.

---
+++ images/setup-scripts/99-curlie.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+version="1.6.0"
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+	rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${tmp_dir}/curlie.tgz" \
+	"https://github.com/rs/curlie/releases/download/v${version}/curlie_${version}_linux_amd64.tar.gz"
+
+tar -C "${tmp_dir}" --no-same-owner -xzf "${tmp_dir}/curlie.tgz"
+mv "${tmp_dir}/curlie" "${installation_dir}"
+chmod +x "${installation_dir}/curlie"
