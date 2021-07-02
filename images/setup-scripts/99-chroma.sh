34fe063fdad06d6e870466010bb6732f62cc6599
Ivan Fraixedes <ivan@fraixed.es>
2021-07-02 10:42:24 +0200

    images: Install Chroma in workspace images
    Add the setup script to install the Chroma tool (terminal syntax
    highlighter) in the workspace images.

---
+++ images/setup-scripts/99-chroma.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+version="0.9.2"
+
+installation_dir=/apps
+mkdir -p ${installation_dir}
+
+tmp_dir=$(mktemp -d)
+cleanup() {
+	rm -rf "${tmp_dir}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${tmp_dir}/chroma.tgz" \
+	"https://github.com/alecthomas/chroma/releases/download/v${version}/chroma-${version}-linux-amd64.tar.gz"
+
+tar -C "${tmp_dir}" --no-same-owner -xzf "${tmp_dir}/chroma.tgz"
+mv "${tmp_dir}/chroma" "${installation_dir}"
+chmod +x "${installation_dir}/chroma"
