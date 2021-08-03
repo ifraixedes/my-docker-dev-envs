e105c8590de17936e27ca939f7bbfe44919523a8
Ivan Fraixedes <ivan@fraixed.es>
2021-08-03 12:44:16 +0200

    images: Add salt to storj image
    Install salt (https://saltproject.io/) to the Storj workspace image.

---
+++ images/setup-scripts/storj/70-salt.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# Download key
+curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
+# Create apt sources list file
+echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | tee /etc/apt/sources.list.d/salt.list
+
+apt-get update
+apt-get install -y salt-ssh python3-googleapi
