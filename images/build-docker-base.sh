47cee1b1abff193ade0ab1b8d15cf98976391528
Ivan Fraixedes <ivan@fraixed.es>
2021-08-11 16:45:28 +0200

    images: Configure sudo on base image
    My base image had already sudo installed because the official Ubuntu
    20.04 image has it, however I was not using it and I used the `su`
    command when I needed to run something with root privileges.
    
    Now, I have the need to run quite often several commands with root
    privileges for Storj, hence, having sudo is more comfortable and
    productive rather than having to log in as root and not having all the
    shell configurations that my usual user has.
    
    This commit changes the base image to make sure that sudo is installed
    and configure it to be used by the normal user under a password because
    I prefer for security reasons not allow to elevate the user privileges
    without typing a password.

--- images/build-docker-base.sh
+++ images/build-docker-base.sh
@@ -5,20 +5,21 @@ set -eu -o pipefail
 # configuration parameters
 user_name=$(id -un)
 user_id=$(id -u)
+user_pwd=
 
-# Root's password to use
-if [[ "$#" -gt 0 ]]; then
-  root_pwd="${1}"
+# User's password to use
+if [[ "$#" -ge 1 ]]; then
+	user_pwd="${1}"
 else
-  read -s -p "specify password for root user: " root_pwd
+	read -r -s -p "specify password for ${user_name}  user: " user_pwd
 fi
 
 docker build \
-  --force-rm \
-  --no-cache \
-  --pull \
-  --build-arg USER_NAME="${user_name}" \
-  --build-arg USER_ID="${user_id}" \
-  --build-arg ROOT_PWD="${root_pwd}" \
-  -f Dockerfile.base \
-  -t ifraixedes/ubuntu/base:20.04 .
+	--force-rm \
+	--no-cache \
+	--pull \
+	--build-arg USER_NAME="${user_name}" \
+	--build-arg USER_ID="${user_id}" \
+	--build-arg USER_PWD="${user_pwd}" \
+	-f Dockerfile.base \
+	-t ifraixedes/ubuntu/base:20.04 .
