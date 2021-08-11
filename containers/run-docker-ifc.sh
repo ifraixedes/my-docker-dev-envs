06a823657468a78aaeb75d82ffd9050ff925b7ae
Ivan Fraixedes <ivan@fraixed.es>
2021-08-11 16:52:55 +0200

    containers: Homogenize run shell scripts
    Homogenize the shell scripts to run the workspace containers for being
    more close to shell script good practices.

--- containers/run-docker-ifc.sh
+++ containers/run-docker-ifc.sh
@@ -7,26 +7,26 @@ repo_remote="${1}"
 path_dir_container_script="${2}"
 
 # This variable is used for identifying docker image, volumes, git branches,etc.
-id=ifc
+readonly ID=ifc
 
-volume_name="ifraixedes-ubuntu-${id}"
+volume_name="ifraixedes-ubuntu-${ID}"
 if ! docker volume ls | grep ${volume_name} >/dev/null; then
 	docker volume create ${volume_name}
 fi
 
 user_name=$(id -un)
-repo_branch="${id}"
+repo_branch="${ID}"
 persistent_dir="/home/${user_name}/persistent"
 
-docker run --name ifraixedes-${id} \
+docker run --name ifraixedes-${ID} \
 	--rm \
 	-ti \
 	--user "${user_name}:${user_name}" \
-	--hostname if${id} \
+	--hostname if${ID} \
 	--network=host \
 	--mount type=volume,src="${volume_name}",dst=/home/"${user_name}"/persistent \
 	--mount type=bind,src="${HOME}",dst=/hostmachine \
-	ifraixedes/ubuntu/${id}:20.04 \
+	ifraixedes/ubuntu/${ID}:20.04 \
 	zsh -c \
 	"${path_dir_container_script}/init-ifc.sh ${persistent_dir} ${repo_remote} ${repo_branch} && \
     ${path_dir_container_script}/entrypoint-ifc.sh"
--- containers/run-docker-storj.sh
+++ containers/run-docker-storj.sh
@@ -7,15 +7,15 @@ repo_remote="${1}"
 path_dir_container_script="${2}"
 
 # This variable is used for identifying docker image, volumes, git branches,etc.
-id=storj
+readonly ID=storj
 
-volume_name="ifraixedes-ubuntu-${id}"
+volume_name="ifraixedes-ubuntu-${ID}"
 if ! docker volume ls | grep ${volume_name} >/dev/null; then
 	docker volume create ${volume_name}
 fi
 
 user_name=$(id -un)
-repo_branch="${id}"
+repo_branch="${ID}"
 persistent_dir="/home/${user_name}/persistent"
 
 # group isn't passed to --user flag because it allows to use all the groups that
@@ -24,11 +24,11 @@ persistent_dir="/home/${user_name}/persistent"
 #
 # About some of the Linux capabilities:
 # * NET_ADMIN and NET_BIND_SERVICE are required by sshuttle
-docker run --name ifraixedes-${id} \
+docker run --name ifraixedes-${ID} \
 	--rm \
 	-ti \
 	--user "${user_name}" \
-	--hostname if${id} \
+	--hostname if${ID} \
 	--network=host \
 	--mount type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
 	--mount type=volume,src="${volume_name}",dst=/home/"${user_name}"/persistent \
@@ -37,7 +37,7 @@ docker run --name ifraixedes-${id} \
 	--cap-add=NET_ADMIN \
 	--cap-add=NET_BIND_SERVICE \
 	--security-opt seccomp=unconfined \
-	ifraixedes/ubuntu/${id}:20.04 \
+	ifraixedes/ubuntu/${ID}:20.04 \
 	zsh -c \
 	"${path_dir_container_script}/private/init-storj.sh ${persistent_dir} ${repo_remote} ${repo_branch} && \
     ${path_dir_container_script}/entrypoint-storj.sh"
