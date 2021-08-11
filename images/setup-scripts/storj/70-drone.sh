aaa1460971086e5d7658393244f28f2f1d473af2
Ivan Fraixedes <ivan@fraixed.es>
2021-08-11 13:39:13 +0200

    images: Install drone CI cli Storj image
    Install the command line interface binary for Drone CI in the Storj
    workspace image.

---
+++ images/setup-scripts/storj/70-drone.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+readonly VERSION=latest
+readonly INSTALLATION_DIR=/apps
+mkdir -p ${INSTALLATION_DIR}
+
+readonly BIN_NAME="drone"
+readonly DONWLOAD_URL="https://github.com/drone/drone-cli/releases/${VERSION}/download/drone_linux_amd64.tar.gz"
+
+TMP_DIR=$(mktemp -d)
+readonly TMP_DIR
+cleanup() {
+	rm -rf "${TMP_DIR}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${TMP_DIR}/${BIN_NAME}.tgz" \
+	"${DONWLOAD_URL}"
+
+cd "${TMP_DIR}"
+tar xzf "${TMP_DIR}/${BIN_NAME}.tgz" --no-same-owner
+chmod +x "${BIN_NAME}"
+mv "${BIN_NAME}" "${INSTALLATION_DIR}/"
