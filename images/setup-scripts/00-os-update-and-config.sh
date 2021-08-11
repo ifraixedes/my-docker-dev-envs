e0b0018f76cfb4a9216328d3fd0cc1beefea40d9
Ivan Fraixedes <ivan@fraixed.es>
2021-08-11 16:45:28 +0200

    images: Configure sudo on base image
    My base image had already sudo installed because the official Ubuntu
    20.04 image has it, however I was not using it and I used the `su`
    command when I needed to run something with root privileges.
    
    Now, I have the need to run quite often several commands with root
    privileges for Storj, hence, having sudo is more comfortable and
    productive rather than having to log in as root and not having all the
    shell configurations that my usual user has.
    
    This commit changes the base image to make sure that sudo is installed
    and configure it to be used by the normal user under a password because
    I prefer for security reasons not allow to elevate the user privileges
    without typing a password.

--- images/setup-scripts/00-os-update-and-config.sh
+++ images/setup-scripts/00-os-update-and-config.sh
@@ -3,10 +3,25 @@
 set -eu -o pipefail
 
 # Required arguments
-locale=${1}
-user_name=${2}
-user_id=${3}
-root_pwd=${4}
+readonly LOCALE="${1:-}"
+readonly USER_NAME="${2:-}"
+readonly USER_ID="${3:-}"
+readonly USER_PWD="${4:-}"
+
+declare -A args
+args=(
+	["1st argument (locale)"]="${LOCALE}"
+	["2nd argument (user name)"]="${USER_NAME}"
+	["3rd argument (user ID)"]="${USER_ID}"
+	["4th argument (user password)"]="${USER_PWD}"
+)
+
+for arg in "${!args[@]}"; do
+	if [ -z "${args[$arg]}" ]; then
+		echo "${arg} must be passed and cannot be empty"
+		exit 1
+	fi
+done
 
 # Update OS
 apt-get update -y
@@ -14,17 +29,19 @@ apt-get upgrade -y
 
 # Configure locale
 apt-get install -y locales
-locale-gen ${locale}
+locale-gen ${LOCALE}
 
 # Configure local time
 apt-get install -y tzdata
 ln -fs /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
-# Configure root account
-# set a password to the root account for being able to use `su` command
-echo root:"${root_pwd}" | chpasswd
-chsh --shell /bin/bash
+# Make sure to install sudo
+apt-get install -y sudo
 
-# Create normal user
-useradd --create-home --user-group --uid "${user_id}" "${user_name}"
+# Create the user account
+useradd --create-home --user-group --uid "${USER_ID}" -G sudo "${USER_NAME}"
+echo "${USER_NAME}":"${USER_PWD}" | chpasswd
+
+# Allow the user to get root privileges
+echo "${USER_NAME}    ALL=(ALL:ALL) ALL" >>"/etc/sudoers.d/99-user-${USER_NAME}"
