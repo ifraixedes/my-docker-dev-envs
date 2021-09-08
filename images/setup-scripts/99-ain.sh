95c2374025153450fc31bf0b3f62b62d4395d224
Ivan Fraixedes <ivan@fraixed.es>
2021-09-08 16:03:03 +0200

    images: Create Ain setup & add it to Storj
    Create a setup script to download the Ain, HTTP client similar to
    Postman, and install it in the Storj workspace image.

---
+++ images/setup-scripts/99-ain.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+readonly VERSION="1.1.0"
+readonly INSTALLATION_DIR="/apps"
+mkdir -p "${INSTALLATION_DIR}"
+
+readonly BIN_NAME="ain"
+readonly DOWNLOAD_URL="https://github.com/jonaslu/ain/releases/download/v${VERSION}/ain_${VERSION}_linux_x86_64.tar.gz"
+
+TMP_DIR=$(mktemp -d)
+readonly TMP_DIR
+cleanup() {
+	trap - EXIT
+
+	rm -rf "${TMP_DIR}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${TMP_DIR}/${BIN_NAME}.tgz" \
+	"${DOWNLOAD_URL}"
+
+cd "${TMP_DIR}"
+tar zxf "${BIN_NAME}.tgz" --strip-components 1 --no-same-owner
+chmod +x "${BIN_NAME}"
+mv "${BIN_NAME}" "${INSTALLATION_DIR}/"
