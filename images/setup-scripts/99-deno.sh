701dbe12fb8a0d36403afa0cf733f619dfddfaf9
Ivan Fraixedes <ivan@fraixed.es>
2021-11-19 16:18:56 +0100

    images: Install Deno in ifc
    Create a script for installing Deno and install it in the ifc workspace
    image.

---
+++ images/setup-scripts/99-deno.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+readonly VERSION="1.16.2"
+readonly INSTALLATION_DIR="/apps"
+mkdir -p "${INSTALLATION_DIR}"
+
+readonly BIN_NAME="deno"
+readonly DOWNLOAD_URL="https://github.com/denoland/deno/releases/download/v${VERSION}/deno-x86_64-unknown-linux-gnu.zip"
+
+TMP_DIR=$(mktemp -d)
+readonly TMP_DIR
+cleanup() {
+	rm -rf "${TMP_DIR}"
+}
+trap cleanup EXIT
+
+curl --fail -L \
+	-o "${TMP_DIR}/${BIN_NAME}.zip" \
+	"${DOWNLOAD_URL}"
+
+unzip -j "${TMP_DIR}/${BIN_NAME}.zip" -d "${INSTALLATION_DIR}"
+chmod +x "${INSTALLATION_DIR}/${BIN_NAME}"
