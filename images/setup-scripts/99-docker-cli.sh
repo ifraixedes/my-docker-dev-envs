963cfd612d9830a58ab5b4f838b1cb0c43864aaf
Ivan Fraixedes <ivan@fraixed.es>
2022-07-09 19:00:55 +0200

    images: Update Docker CLI installation
    The Docker client installation has changed to use a Linux repository.
    The previous one was working but it provoked `apt` to always show a
    warning due to use an PGP key for the repository.
    
    Using the official way currently published suppress the `apt` warning.

--- images/setup-scripts/99-docker-cli.sh
+++ images/setup-scripts/99-docker-cli.sh
@@ -12,10 +12,24 @@ docker_gid=${2}
 groupadd -g "${docker_gid}" docker
 usermod -a -G docker "${user_name}"
 
-# Add docker APT repository requirements
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu disco stable" >/etc/apt/sources.list.d/docker-engine-community.list
+# Install required dependency packages.
+# some of this dependencies allow apt to use a repository over HTTPS.
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
+# Add Docker’s official GPG key.
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Set up the Docker repository.
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install the Docker client.
 apt-get update
-apt-get install -y docker-ce-cli docker-compose
+apt-get install -y docker-ce-cli docker-compose-plugin
