5d077de3a5aed2444f3413f741be8c378233700a
Ivan Fraixedes <ivan@fraixed.es>
2022-09-07 19:28:36 +0200

    images: Don't allow to install salt version 3005
    Force to install the salt-ssh version 3004 in the Storj workspace image
    because the new 3005 doesn't work properly with Storj infra
    configuration.

--- images/setup-scripts/storj/70-salt.sh
+++ images/setup-scripts/storj/70-salt.sh
@@ -6,4 +6,4 @@ curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproj
 echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" | tee /etc/apt/sources.list.d/salt.list
 
 apt-get update
-apt-get install -y salt-ssh python3-googleapi
+apt-get install -y salt-common=3004.1+dfsg-2 salt-ssh=3004.1+dfsg-2 python3-googleapi
