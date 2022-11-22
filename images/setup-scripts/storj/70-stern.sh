468f1835bb1c7cf2418b2acd508578534871993e
Ivan Fraixedes <ivan@fraixed.es>
2022-11-22 13:33:34 +0100

    images: Update stern setup script & bump vesion
    Stern is migrated from a Github user to a Github organization and the
    URL didn't work anymore.
    
    This commit updates the URL and updates the script to use the new format
    that I'm using for the shell scripts.

--- images/setup-scripts/storj/70-stern.sh
+++ images/setup-scripts/storj/70-stern.sh
@@ -1,12 +1,26 @@
-#!/usr/bin/env -S bash -
+#!/usr/bin/env bash
 
 set -eux -o pipefail
 
-installation_dir=/apps
-mkdir -p ${installation_dir}
+readonly VERSION="1.22.0"
+readonly INSTALLATION_DIR=/apps
+mkdir -p ${INSTALLATION_DIR}
+
+readonly BIN_NAME="stern"
+readonly DONWLOAD_URL="https://github.com/stern/stern/releases/download/v${VERSION}/stern_${VERSION}_linux_amd64.tar.gz"
+
+TMP_DIR=$(mktemp -d)
+readonly TMP_DIR
+cleanup() {
+	rm -rf "${TMP_DIR}"
+}
+trap cleanup EXIT
 
 curl --fail -L \
-	-o "${installation_dir}/stern" \
-	"https://github.com/wercker/stern/releases/download/1.11.0/stern_linux_amd64"
+	-o "${TMP_DIR}/${BIN_NAME}.tgz" \
+	"${DONWLOAD_URL}"
 
-chmod +x "${installation_dir}/stern"
+cd "${TMP_DIR}"
+tar xzf "${BIN_NAME}.tgz"
+chmod +x "${BIN_NAME}"
+mv "${BIN_NAME}" "${INSTALLATION_DIR}/"
