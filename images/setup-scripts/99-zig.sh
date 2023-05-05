c35816d920abe5e25fdf79cb4ef6cce638813e72
Ivan Fraixedes <ivan@fraixed.es>
2023-05-05 16:28:39 +0200

    images: Add Zig prog lang to IFC workspace image

---
+++ images/setup-scripts/99-zig.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eux -o pipefail
+
+readonly VERSION="0.10.1"
+readonly INSTALLATION_DIR="/apps"
+mkdir -p "${INSTALLATION_DIR}"
+
+readonly BIN_NAME="zig"
+readonly DOWNLOAD_URL="https://ziglang.org/download/${VERSION}/zig-linux-x86_64-${VERSION}.tar.xz"
+
+TMP_DIR=$(mktemp -d)
+readonly TMP_DIR
+
+curl --fail -L \
+	-o "${TMP_DIR}/${BIN_NAME}.tar.xz" \
+	"${DOWNLOAD_URL}"
+
+pushd "${TMP_DIR}"
+tar xf "${BIN_NAME}.tar.xz" --no-same-owner --strip-components=1
+chmod +x "${BIN_NAME}"
+popd
+mv "${TMP_DIR}" "${INSTALLATION_DIR}/${BIN_NAME}"
+chmod +rx "${INSTALLATION_DIR}/${BIN_NAME}"
