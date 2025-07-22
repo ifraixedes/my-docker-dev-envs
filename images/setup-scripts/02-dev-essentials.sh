7c724875e0dd33f80623cebe75edb85a2a32ff95
Ivan Fraixedes <ivan@fraixed.es>
2025-07-22 15:16:33 +0200

    images: Don't install silver searcher (ag)
    Don't install "silver searcher" (a.k.a. ag) because I use ripgrep.

--- images/setup-scripts/02-dev-essentials.sh
+++ images/setup-scripts/02-dev-essentials.sh
@@ -4,7 +4,3 @@ set -eux -o pipefail
 
 # Install general compilers and building tools
 apt-get install -y g++ dh-autoreconf pkg-config
-
-# Install code tools
-## https://github.com/ggreer/the_silver_searcher
-apt-get install -y silversearcher-ag
