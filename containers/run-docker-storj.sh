c5d0e10845386c1ca94640160b262b1f754865e9
Ivan Fraixedes <ivan@fraixed.es>
2025-11-19 11:37:43 +0100

    containers: Allow to exec desktops apps
    Allow to execute desktop apps installed in the Storj image through the
    host machine X11 server.

--- containers/run-docker-storj.sh
+++ containers/run-docker-storj.sh
@@ -19,30 +19,35 @@ user_name=$(id -un)
 repo_branch="${ID}"
 persistent_dir="/home/${user_name}/persistent"
 
+# Allow docker user to connect to X11 socket to be able to run desktop apps installed in the image
+# through the host machine.
+xhost +local:docker
+
 # group isn't passed to --user flag because it allows to use all the groups that
 # the host machine user belongs. This is useful to be able to use docker-cli
 # installed in the container connecting to the host docker daemon.
 # So do `--user "${user_name}"` instead of `--user "${user_name}:${user_name}"`.
 docker run --name ifraixedes-${ID} \
-	--rm \
-	-ti \
-	--user "${user_name}" \
-	--hostname if${ID} \
-	--add-host=if${ID}:127.0.0.1 \
-	--network=host \
-	--mount "type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock" \
-	--mount "type=volume,src=${volume_name},dst=/home/${user_name}/persistent" \
-	--mount "type=bind,src=${HOME}/workspace,dst=/hostmachine/workspace" \
-	--mount "type=bind,src=${path_dir_container_scripts},dst=/hostmachine/container-scripts,readonly" \
-	--mount "type=bind,src=${path_dir_container_scripts_private},dst=/hostmachine/container-scripts-private,readonly" \
-	--mount "type=bind,src=${repo_remote},dst=/hostmachine/container-home.git" \
-	--mount "type=bind,src=${HOME}/.Xauthority,dst=/home/${user_name}/.Xauthority" \
-	--env DISPLAY="${DISPLAY}" \
-	--cap-add=SYS_PTRACE \
-	--security-opt seccomp=unconfined \
-	ifraixedes/ubuntu/${ID}:22.04 \
-	zsh -c \
-	"/hostmachine/container-scripts-private/init-${ID}.sh ${persistent_dir} /hostmachine/container-home.git ${repo_branch} && \
+ --rm \
+ -ti \
+ --user "${user_name}" \
+ --hostname if${ID} \
+ --add-host=if${ID}:127.0.0.1 \
+ --network=host \
+ --mount "type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock" \
+ --mount "type=volume,src=${volume_name},dst=/home/${user_name}/persistent" \
+ --mount "type=bind,src=${HOME}/workspace,dst=/hostmachine/workspace" \
+ --mount "type=bind,src=${path_dir_container_scripts},dst=/hostmachine/container-scripts,readonly" \
+ --mount "type=bind,src=${path_dir_container_scripts_private},dst=/hostmachine/container-scripts-private,readonly" \
+ --mount "type=bind,src=${repo_remote},dst=/hostmachine/container-home.git" \
+ --mount "type=bind,src=${HOME}/.Xauthority,dst=/home/${user_name}/.Xauthority" \
+ --mount "type=bind,src=/tmp/.X11-unix,dst=/tmp/.X11-unix" \
+ --env DISPLAY="${DISPLAY}" \
+ --cap-add=SYS_PTRACE \
+ --security-opt seccomp=unconfined \
+ ifraixedes/ubuntu/${ID}:22.04 \
+ zsh -c \
+ "/hostmachine/container-scripts-private/init-${ID}.sh ${persistent_dir} /hostmachine/container-home.git ${repo_branch} && \
     /hostmachine/container-scripts/entrypoint-${ID}.sh"
 
 # The bind to .Xautority and the DISPLAY environment variable mapping are for
